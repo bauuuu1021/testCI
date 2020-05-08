6bcd666211b13a37f4bd57590406094ca5c9f66a
bauuuu1021 <bboy010554@icloud.com>
2020-05-09 02:05:51 +0800

    plz

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,8 +7,9 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
-
+/*
     protected function setUp(): void
     {
     }
+*/
 }
